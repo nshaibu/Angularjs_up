2007bd81126b61f2ebe9da7d0517c854eb738a7d
nshaibu <nafiushaibu1@gmail.com>
2020-04-25 14:56:07 +0000

    add service

--- angular-app/src/app/heroes/heroes.component.ts
+++ angular-app/src/app/heroes/heroes.component.ts
@@ -9,8 +9,6 @@ import { MessageService } from '../message.service'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(
@@ -20,11 +18,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onselectHero(hero: Hero): void {
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
-    this.selectedHero = hero
   }
 
   getHeroes(): void {
